f88eb99c695b05fec5d29790abcca05a0f19912a
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-01-18 23:48:03 +0530

    Added solution - LeetHub

--- Rotate Array - GFG/rotate-array.java
+++ Rotate Array - GFG/rotate-array.java
@@ -5,25 +5,30 @@ import java.lang.*;
 import java.io.*;
 
 class GFG {
-	public static void main (String[] args) {
+	public static void main (String[] args) 
+	{
 		//code
 		 Scanner sc=new Scanner(System.in);
          int T=sc.nextInt();
-         while(T>0){
-         int N=sc.nextInt();
-         int D=sc.nextInt();
-         int A[]=new int[N];
-         for(int i=0;i<N;i++){
-             A[i]=sc.nextInt();
-         }
-          for(int i=D;i<N;i++){
+         while(T>0)
+         {
+             int N=sc.nextInt();
+             int D=sc.nextInt();
+             int A[]=new int[N];
+             for(int i=0;i<N;i++)
+             {
+                 A[i]=sc.nextInt();
+             }
+          for(int i=D;i<N;i++)
+          {
              System.out.print(A[i] + " ");
-         }
-          for(int i=0;i<D;i++){
+          }
+          for(int i=0;i<D;i++)
+          {
              System.out.print(A[i] +" ");
-         }
+          }
          System.out.println();
          T--;
-         }
+       }
 	}
 }
