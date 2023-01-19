8a7671c16526a7003847fcc5eb3bb69d724d1e99
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-01-19 20:33:02 +0530

    Added solution - LeetHub

---
+++ Carpet into Box - GFG/carpet-into-box.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int A =sc.nextInt();
+            int B =sc.nextInt();
+            int C =sc.nextInt();
+            int D =sc.nextInt();
+             
+           System.out.println(new Solution().carpetBox(A,B,C,D)); 
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    int carpetBox(int A, int B, int C, int D) 
+    { 
+      if((A<=C&&B<=D) || (B<=C&&A<=D))
+        {
+            return 0;  
+        } 
+        if(A>C && A>D)
+        {
+            return 1+carpetBox(A/2,B,C,D);  
+        } 
+        if(B>C && B>D)
+        {
+            return 1+carpetBox(A,B/2,C,D);
+        } 
+        return Math.min(1+carpetBox(A/2,B,C,D), 1+carpetBox(A,B/2,C,D));
+        
+    }
+   
+}
