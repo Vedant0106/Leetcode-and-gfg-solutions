ba57e8a61e0afab658372814660e4034c1c99dac
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-01-30 23:34:31 +0530

    Added solution - LeetHub

--- Find the element that appears once in sorted array - GFG/find-the-element-that-appears-once-in-sorted-array.java
+++ Find the element that appears once in sorted array - GFG/find-the-element-that-appears-once-in-sorted-array.java
@@ -35,15 +35,10 @@ class Solution
     {
         // Complete this function
           int ans=0;
-
             for(int i=0;i<n;i++)
-
             {
-
                 ans^=arr[i];
-
             }
-
             return ans;
     }
 }
