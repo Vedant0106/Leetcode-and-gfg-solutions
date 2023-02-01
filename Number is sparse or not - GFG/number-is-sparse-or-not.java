2c05155aebb30f2ce2c718af1f58a2ebd57e536f
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-02-01 16:37:01 +0530

    Added solution - LeetHub

--- Number is sparse or not - GFG/number-is-sparse-or-not.java
+++ Number is sparse or not - GFG/number-is-sparse-or-not.java
@@ -15,7 +15,8 @@ class Solution
     public static boolean isSparse(int n)
     {
         // Your code here
-        if((n & (n << 1)) == 0) return true;
+        if((n & (n << 1)) == 0) 
+        return true;
         return false;
     }
     
