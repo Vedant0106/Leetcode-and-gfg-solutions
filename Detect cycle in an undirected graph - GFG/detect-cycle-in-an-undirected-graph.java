80eface1d030c62ff9685a0079b3639975db120f
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-02-09 21:15:20 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -34,18 +34,29 @@ class GFG {
 
 class Solution {
     
-    public boolean dfs(int node , int parent , boolean[] vis , ArrayList<ArrayList<Integer>> adj){
-        if(vis[node])    return true;
+    public boolean dfs(int node , int parent , boolean[] vis , ArrayList<ArrayList<Integer>> adj)
+    {
+        if(vis[node])    
+        return true;
+        
         vis[node] = true;
         for(Integer adjNode : adj.get(node))    
-            if(adjNode!=parent && dfs(adjNode , node , vis , adj))    return true;
+        {
+            if(adjNode!=parent && dfs(adjNode , node , vis , adj))    
+            return true;
+        }
         return false;
     }
     // Function to detect cycle in an undirected graph.
-    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
         // Code here
         boolean[] vis = new boolean[V];
-        for(int i=0;i<V;i++)    if(!vis[i] && dfs(i,-1,vis,adj))    return true;
+        for(int i=0;i<V;i++)    
+        {
+            if(!vis[i] && dfs(i,-1,vis,adj))    
+            return true;
+        }
         return false;
     }
 }
